17e1d67d99d4ae0534747129e4e92ba95acb73d3
Sterben4444 <anthonyho992@gmail.com>
2024-01-20 13:21:34 -0700

    Making login page
    auth-context page made and working on login and create user account pages.

--- app/login/page.js
+++ app/login/page.js
@@ -1,10 +1,29 @@
+"use client";
+import {useUserAuth} from "app/util/security/auth-context";
 
-export default function Home() {
+export default function Page() {
+    const {user, signInWithEmailAndPassword signOut} = useUserAuth();
+
+    function handleSignIn() {
+        
+    }
+
+    function handleSignOut() {
+        signOut();
+    }
+
     return(
-        <main>
-            <div>Testing 123</div>
-            <div>Hello</div>
-        </main>
-    );
-    
+        <div>
+            {!user && <button onClick={handleSignIn}>Sign In</button>}
+            {user && (
+                <div>
+                    <p>
+                        Welcome, {user.displayName ? user.displayName : user.email} 
+                    </p>
+                    <button onClick={handleSignOut}>Sign Out</button>
+                    <div>{user.email}</div>
+                </div>
+            )}
+        </div>
+    )
 }

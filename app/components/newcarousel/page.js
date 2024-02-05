c0c056ac5299214f6c22e2ae0e0fd907ddaee524
Yushu0 <119829083+Yushu0@users.noreply.github.com>
2024-02-04 23:33:45 -0700

    carousel

--- app/components/newcarousel/page.js
+++ app/components/newcarousel/page.js
@@ -1,90 +1,111 @@
-"use client"
-import React, { useState } from "react"
-import { useKeenSlider } from "keen-slider/react"
-import "keen-slider/keen-slider.min.css"
-import "./styles.css"
-import Image from "next/image"
+'use client';
+import {Carousel} from "@material-tailwind/react";
 
-export default () => {
-  const [currentSlide, setCurrentSlide] = useState(0)
-  const [loaded, setLoaded] = useState(false)
-  const [sliderRef, instanceRef] = useKeenSlider({
-    initial: 0,
-    slideChanged(slider) {
-      setCurrentSlide(slider.track.details.rel)
-    },
-    created() {
-      setLoaded(true)
-    },
-  })
+export default function CarouselDefault() {
+  const theme = {
+    carousel: {
+      defaultProps: {
+        prevArrow: ({loop, handlePrev, firstIndex}) => {
+          return (
+            <button
+              onClick={handlePrev}
+              disabled={!loop && firstIndex}
+              className="!absolute top-2/4 left-4 -translate-y-2/4 rounded-full select-none transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] text-white hover:bg-white/10 active:bg-white/30 grid place-items-center"
+            >
+              {/*<ChevronLeftIcon strokeWidth={3} className="-ml-1 h-7 w-7" />*/}
+            </button>
+          );
+        },
+        nextArrow: ({loop, handleNext, lastIndex}) => (
+          <button
+            onClick={handleNext}
+            disabled={!loop && lastIndex}
+            className="!absolute top-2/4 right-4 -translate-y-2/4 rounded-full select-none transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-12 max-w-[48px] h-12 max-h-[48px] text-white hover:bg-white/10 active:bg-white/30 grid place-items-center"
+          >
+            {/*<ChevronRightIcon strokeWidth={3} className="ml-1 h-7 w-7" />*/}
+          </button>
+        ),
+        navigation: ({setActiveIndex, activeIndex, length}) => (
+          <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+            {new Array(length).fill("").map((_, i) => (
+              <span
+                key={i}
+                className={`block h-3 w-3 cursor-pointer rounded-full transition-colors content-[''] ${
+                  activeIndex === i ? "bg-white" : "bg-white/50"
+                }`}
+                onClick={() => setActiveIndex(i)}
+              />
+            ))}
+          </div>
+        ),
+        autoplay: true,
+        autoplayDelay: 5000,
+        transition: {
+          type: "tween",
+          duration: 0.5,
+        },
+        loop: false,
+        className: "",
+      },
+      styles: {
+        base: {
+          carousel: {
+            position: "relative",
+            width: "w-full",
+            height: "h-full",
+            overflowX: "overflow-x-hidden",
+            overflowY: "overflow-y-hidden",
+            display: "flex",
+          },
 
+          slide: {
+            width: "w-full",
+            height: "h-full",
+            display: "inline-block",
+            flex: "flex-none",
+          },
+        },
+      },
+    },
+  }
   return (
-    <>
-      <div className="relative">
-        <div ref={sliderRef} className="keen-slider">
-          <div className="keen-slider__slide number-slide1"><Image className="image-properties" src="/img/Kitty_3.png" fill/></div>
-          <div className="keen-slider__slide number-slide2"><Image className="image-properties" src="/img/Kitty_4.png" fill/></div>
-        </div>
-        {loaded && instanceRef.current && (
-          <>
-            <Arrow
-              left
-              onClick={(e) =>
-                e.stopPropagation() || instanceRef.current?.prev()
-              }
-              disabled={currentSlide === 0}
-            />
-
-            <Arrow
-              onClick={(e) =>
-                e.stopPropagation() || instanceRef.current?.next()
-              }
-              disabled={
-                currentSlide ===
-                instanceRef.current.track.details.slides.length - 1
-              }
-            />
-          </>
+    <div className="grid max-h-[440px] w-full place-items-center overflow-x-hidden overflow-y-hidden p-6">
+      <Carousel
+        className="pb-5"
+        autoplay
+        loop
+        navigation={({setActiveIndex, activeIndex, length}) => (
+          <div className="absolute bottom-0 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+            {new Array(length).fill("").map((_, i) => (
+              <span
+                key={i}
+                className={`block h-3 w-3 cursor-pointer rounded-full transition-colors content-[''] ${
+                  activeIndex === i ? "bg-black" : "bg-gray-300"
+                }`}
+                onClick={() => setActiveIndex(i)}
+              />
+            ))}
+          </div>
         )}
-      </div>
-      {loaded && instanceRef.current && (
-        <div className="flex py-2 justify-center">
-          {[
-            ...Array(instanceRef.current.track.details.slides.length).keys(),
-          ].map((idx) => {
-            return (
-              <button
-                key={idx}
-                onClick={() => {
-                  instanceRef.current?.moveToIdx(idx)
-                }}
-                className={"dot" + (currentSlide === idx ? " active" : "")}
-              ></button>
-            )
-          })}
-        </div>
-      )}
-    </>
-  )
-}
 
-function Arrow(props) {
-  const disabled = props.disabled ? " arrow--disabled" : ""
-  return (
-    <svg
-      onClick={props.onClick}
-      className={`arrow ${
-        props.left ? "arrow--left" : "arrow--right"
-      } ${disabled}`}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      {props.left && (
-        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
-      )}
-      {!props.left && (
-        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
-      )}
-    </svg>
+      >
+        <img
+          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+          alt="image 1"
+          className="h-full w-full object-cover"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+          alt="image 2"
+          className="h-full w-full object-cover"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+          alt="image 3"
+          className="h-full w-full object-cover"
+        />
+      </Carousel>
+    </div>
   )
+    ;
 }

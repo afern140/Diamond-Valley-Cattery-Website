83be515024e784db45a6b6415577fb48b0609b0d
tansimafiana <tansima.fiana@edu.sait.ca>
2024-02-05 11:45:05 -0700

    add Carousel

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import CarouselDefault from './components/carousel';
 import Button from './components/displaybutton'
 
 export default function Home() {
   return (
     <div className="w-full bg-white">
       <div className="container mx-auto">
-        {/*<Carousel />*/}
+        <CarouselDefault />
 
         <h2 className=" text-4xl flex text-center justify-center text-black py-4">Welcome to Diamond Valley Cattery</h2>
         

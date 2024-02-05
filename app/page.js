f4b17c3fa3e8c2b038e44ad87f486fa8cc521cd4
afern <afern@LAPTOP-9NLR21M1>
2024-02-05 08:41:50 -0700

    Fix Carousel

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import CarouselDefault from './components/carousel';
 import Button from './components/displaybutton/page'
 
 export default function Home() {
   return (
     <div className="w-full bg-white">
       <div className="container mx-auto">
-        {/*<Carousel />*/}
+        <CarouselDefault />
 
         <h2 className="font-sans font-normal text-2xl flex items-center justify-center text-center text-black py-4">Welcome to Diamond Valley Cattery</h2>
         

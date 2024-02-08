91fce5e6927f9604a7933bcb0716367be7703aa2
KingCharlesTheGr8th <119707706+KingCharlesTheGr8th@users.noreply.github.com>
2024-02-08 13:04:45 -0700

    Fixes login page

--- app/login/page.js
+++ app/login/page.js
@@ -3,20 +3,19 @@ import { useState } from "react";
 import { useUserAuth } from "../_utils/auth-context";
 import { auth } from "../_utils/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import Chat from "../chat/Chat";
 import Link from "next/link";
+ 
 
 export default function Page() {
-  
-  const {user, firebaseSignOut} = useUserAuth();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const {user, firebaseSignOut} = useUserAuth();
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
 
-  function handleSignOut() {
-    firebaseSignOut();
-  }
+function handleSignOut(){
+     firebaseSignOut();
+}
 
-  function handleEmailPasswordSignIn(e) {
+function handleEmailPasswordSignIn(e){
     e.preventDefault();
     signInWithEmailAndPassword(auth, email, password)
       .catch((error) => {
@@ -32,51 +31,19 @@ export default function Page() {
       });
 }
 
-  return (
+ return(
     <div className="bg-white min-h-screen flex items-center justify-center">
-      {!user && (
+        {!user &&
         <div dir="ltr" className="text-center">
-          <p className="text-xl text-slate-400 font-semibold mb-10">
-            Sign in to your account
-          </p>
-          <form
-            onSubmit={handleEmailPasswordSignIn}
-            className="mb-8 flex flex-col items-center"
-          >
-            <input
-              type="email"
-              value={email}
-              className="text-black border-s-4 border-slate-300 p-2 mb-4"
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email"
-              required
-            />
-            <input
-              type="password"
-              value={password}
-              className="text-black border-s-4 border-slate-300 p-2 mb-4"
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              required
-            />
-            <button
-              type="submit"
-              className="bg-slate-400 active:bg-slate-600 rounded text-white p-2"
-            >
-              Sign In with Email
-            </button>
-          </form>
-          <div>
-            <button
-              onClick={() => auth.sendPasswordResetEmail(email)}
-              className="text-slate-500 mb-2"
-            >
-              Forgot Password
-            </button>
-            <Link href="login/signup" className="text-slate-500  ml-6">
-              Sign Up
-            </Link>
-          </div>
+          <p className="text-xl text-slate-400 font-semibold mb-10">Sign in to your account</p>
+          <form onSubmit={handleEmailPasswordSignIn} className="mb-8 flex flex-col items-center">
+            <input type="email" value={email} className="text-black border-s-4 border-slate-300 p-2 mb-4" onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
+            <input type="password" value={password} className="text-black border-s-4 border-slate-300 p-2 mb-4" onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+            <button type="submit" className="bg-slate-400 active:bg-slate-600 rounded text-white p-2">Sign In with Email</button>
+        </form>
+        <div>
+          <button onClick={() => auth.sendPasswordResetEmail(email)} className="text-slate-500 mb-2">Forgot Password</button>
+          <Link href='login/signup' className="text-slate-500  ml-6">Sign Up</Link>
         </div>
       </div>
         }
@@ -84,17 +51,10 @@ export default function Page() {
         <div className="text-center text-slate-500">
             <p>
             Welcome, {user.displayName} ({user.email})
-          </p>
-          <button
-            onClick={handleSignOut}
-            className="bg-slate-500 text-white p-2"
-          >
-            Sign Out
-          </button>
-          <br />
+            </p>
+            <button onClick={handleSignOut} className="bg-slate-500 text-white p-2">Sign Out</button>
+            <br/>
         </div>
-      )}
-      {user ? <Chat /> : null}
-    </div>
-  );
-}
+        )}
+  </div>
+)}

b736e537114e33eb1de3d62d073057788748dc9f
Yushu0 <119829083+Yushu0@users.noreply.github.com>
2024-03-03 17:46:39 -0700

    homepage for mobile

--- app/page.js
+++ app/page.js
@@ -1,17 +1,17 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import Carousel from './components/newcarousel/page'
-import Button from './components/displaybutton/page'
+import CarouselDefault from './components/carousel';
+import Button from './components/displaybutton'
 
 export default function Home() {
   return (
     <div className="w-full bg-white">
       <div className="container mx-auto">
-        {/*<Carousel />*/}
+        <CarouselDefault />
 
         <h2 className="font-sans font-normal text-2xl flex items-center justify-center text-center text-black py-4">Welcome to Diamond Valley Cattery</h2>
         
-        <div className="flex justify-between py-6">
+        <div className="flex justify-between py-6 md:flex-row flex-col px-6 gap-8">
           <Button />
           <Button />
           <Button />

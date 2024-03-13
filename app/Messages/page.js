9b612cee01329fe795a56251beda1cae2bc8c52b
John Enoch S. Crudo <johncrudo24@gmail.com>
2024-03-13 16:13:26 -0600

    Preparing for major message system
    Preparing to make a Messaging system that allow the user to chat with another user 1 on 1.

---
+++ app/Messages/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../chat/sidebar/sidebar";
+import Chats from "../chat/chats/chats";
+
+const Page = () => {
+    return(
+        <div>
+            <Sidebar />
+            <Chats />
+        </div>
+    )
+}
+
+export default Page;
---
+++ app/chat/inputs/inputs.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const inputs = () => {
+    return(
+        <div>inputs</div>
+    )
+}
+
+export default inputs;

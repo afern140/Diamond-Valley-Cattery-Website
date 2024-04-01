d264cac6d05152b56178bf78b484b527846d6e29
Sterben4444 <anthonyho992@gmail.com>
2024-03-31 22:22:26 -0600

    Added updateUserProfile
    Function to update either user Display name or photo needs to implement user interface don't completly know about updating user

--- app/_utils/auth-context.js
+++ app/_utils/auth-context.js
@@ -8,12 +8,15 @@ import {
   GithubAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  updateProfile,
+  getAuth
 } from "firebase/auth";
 import { auth } from "./firebase";
 import {getUser} from "./user_services";
 import { set } from "firebase/database";
  
 const AuthContext = createContext();
+const auth = getAuth(displayName, photoURL);
  
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
@@ -34,7 +37,7 @@ export const AuthContextProvider = ({ children }) => {
   const firebaseSignOut = () => {
     return signOut(auth);
   };
- 
+  
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
       if (currentUser) {
@@ -53,6 +56,37 @@ export const AuthContextProvider = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
+/*
+  * Updates the user's profile with a new display name and/or photo URL.
+  *
+  * @param {string} newDisplayName - The new display name for the user.
+  * @param {string} newPhotoURL - The new photo URL for the user.
+  * 
+  * If either parameter is provided, it will update the corresponding field in the user's profile.
+  * If the update is successful, an alert will be shown with the message "Profile updated".
+  * If the update fails, an alert will be shown with the message "Profile update failed".
+*/
+export function upateUserProflie(newDisplayName, newPhotoURL){
+  const updateObject = {};
+  if (newDisplayName) {
+    updateObject.displayName = newDisplayName;
+  }
+  if (newPhotoURL) {
+    updateObject.photoURL = newPhotoURL;
+  }
+  if (newDisplayName || newPhotoURL) {
+    updateProfile(auth.currentUser, updateObject)
+      .then(() => {
+        alert ("Profile updated");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Profile update failed");
+      });
+  }
+}
+
  
 export const useUserAuth = () => {
     //console.log(useContext(AuthContext));

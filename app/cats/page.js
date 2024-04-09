28906c41c91048e43cbb35798f89bb9f49f8a506
tansimafiana <tansima.fiana@edu.sait.ca>
2024-04-09 15:30:26 -0600

    addChanges to catList page

--- app/cats/page.js
+++ app/cats/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { getObjects, getObject } from "../_utils/firebase_services";
@@ -26,6 +26,7 @@ export default function Page() {
 	const { user } = useUserAuth();
 	const { dbUser } = useUserAuth();
 
+	// Filter Types
 	const [breedType, setBreedType] = useState("");
 	const [genderType, setGenderType] = useState("");
 	const [ageType, setAgeType] = useState("");
@@ -41,6 +42,15 @@ export default function Page() {
 		{age: "Kitten (0-6 months)"},
 		{age: "Young (6 months - 1 year)"},
 		{age: "Adult (1 year+)"}
+	];
+
+	// Sort by filter values
+	const sortbyGroups = [
+		{group: "Name"},
+		{group: "Breed"},
+		{group: "Gender"},
+		{group: "Age"},
+		{group: "Color"}
 	];
 
 	//Replaces the cats list with the database data when it is loaded
@@ -133,6 +143,7 @@ export default function Page() {
 		if (colorType !== "") {
 			filteredData = filteredData.filter((cat) => Object.values(cat.color).join('').toLowerCase().includes(colorType.toLowerCase()) );
 		}
+
 		//Sort
 		//Because React won't actually update data that is simply sorted, we need to create a new array
 		let sortedData;
@@ -141,26 +152,48 @@ export default function Page() {
 			switch (sortingMethod) {
 				case "Name":
 					sortedData = [...filteredData.sort((a, b) => a.name > b.name)];
+					
+					// -- F -- This sorting method works
+					sortedData = sortedData.sort(function (a, b)  { if (a.name > b.name) return 1;  if (a.name < b.name) return -1;  return 0; })
 					break;
 				case "Breed":
 					sortedData = [...filteredData.sort((a, b) => a.breed > b.breed)];
+					
+					// -- F -- This sorting method works
+					sortedData = sortedData.sort(function (a, b)  { if (a.breed > b.breed) return 1;  if (a.breed < b.breed) return -1;  return 0; })
 					break;
 				case "Gender":
 					sortedData = [...filteredData.sort((a, b) => a.gender > b.gender)];
+					
+					// -- F -- This sorting method works
+					sortedData = sortedData.sort(function (a, b)  { if (a.gender > b.gender) return 1;  if (a.gender < b.gender) return -1;  return 0; })
 					break;
 				case "Age":
 					sortedData = [...filteredData.sort((a, b) => a.age - b.age)];
+					
+					// -- F -- This sorting method works
+					sortedData = sortedData.sort(function (a, b)  { if (a.age > b.age) return 1;  if (a.age < b.age) return -1;  return 0; })
 					break;
 				case "Color":
 					sortedData = [...filteredData.sort((a, b) => a.color > b.color)];
+					
+					// -- F -- This sorting method works
+					sortedData = sortedData.sort(function (a, b)  { if (a.color > b.color) return 1;  if (a.breed < b.breed) return -1;  return 0; })
 					break;
 				default:
 					sortedData = [...filteredData.sort((a, b) => a.color > b.color)];
+					
+					// -- F -- This sorting method works
+					sortedData = sortedData.sort(function (a, b)  { if (a.color > b.color) return 1;  if (a.breed < b.breed) return -1;  return 0; })
 					break;
 			}
 		}
-		else if (filteredData != null && filteredData != undefined)
+		else if (filteredData != null && filteredData != undefined) {
 			sortedData = [...filteredData.sort((a, b) => a.id > b.id)];
+
+			// -- F -- This sorting method works
+			sortedData = sortedData.sort(function (a, b)  { if (a.id > b.id) return 1;  if (a.id < b.id) return -1;  return 0; })
+		}
 		//Update the list
 		setFilteredResults(sortedData);
 		console.log("Filtered Data: ");
@@ -176,6 +209,24 @@ export default function Page() {
 		setFieldInput(field);
 	}
 
+	// -- F -- search bar click away
+    const searchbarRef = useRef(null);
+	useEffect(() => {
+		// only add the event listener when the search bar drop is opened
+        if (!activeAutocomplete) return;
+
+        function handleClick(event) {
+            if (searchbarRef.current && !searchbarRef.current.contains(event.target)) {
+                setActiveAutocomplete(false);
+				console.log("Search bar is off!");
+            }
+        }
+
+        window.addEventListener("mousedown", handleClick);
+        return () => { window.removeEventListener("mousedown", handleClick); }
+	}, [activeAutocomplete]);
+
+	// Main Rendering
 	return (
 		<main className="w-full flex-col justify-center pointer-events-auto text-black text-xl font-normal overflow-hidden relative">
 			<BackButton url="#Navbar" />
@@ -194,11 +245,11 @@ export default function Page() {
 		<div className="flex w-full">
 		{/* First split of the page */}
 		<div className=" w-1/3 mr-6 ml-20 align-middle justify-start flex-col flex items-center relative z-20">
-			<div className="p-6 w-full text-text-header-0 rounded-xl relative -z-20 bg-white dark:bg-gray-500  drop-shadow-lg">
+			<div className="p-6 w-fit text-text-header-0 rounded-xl relative -z-20 bg-white dark:bg-gray-500  drop-shadow-lg">
 				<h2 className="py-6 text-2xl font-semibold text-center drop-shadow-md">Filters</h2>
 				<h3 className="py-2 text-lg">Breed</h3>
 				{/*<Dropdown queryType="breed" callback={filterItems} cats={cats} isInsidePanel={true}/>*/}
-				<select id="sort" value={breedType} onChange={(e) => setBreedType(e.target.value)} className=" drop-shadow-md p-2 text-xl rounded-xl bg-[#e5e5ff] bg-opacity-100">
+				<select id="sort" value={breedType} onChange={(e) => setBreedType(e.target.value)} className=" drop-shadow-md p-2 text-xl rounded-xl bg-[#e5e5ff] bg-opacity-100 w-full max-w-[300px]">
 					<option value="">None</option>
 					{breeds.map((breed) => (
 						<option value={breed}>{breed}</option>
@@ -206,14 +257,14 @@ export default function Page() {
 				</select>
 				<h3 className="py-2 text-lg">Gender</h3>
 				{/*<Dropdown queryType="gender" callback={filterItems} cats={cats} isInsidePanel={true}/>*/}
-				<select id="sort" value={genderType} onChange={(e) => setGenderType(e.target.value)} className=" drop-shadow-md p-2 text-xl rounded-xl bg-[#e5e5ff] bg-opacity-100">
+				<select id="sort" value={genderType} onChange={(e) => setGenderType(e.target.value)} className=" drop-shadow-md p-2 text-xl rounded-xl bg-[#e5e5ff] bg-opacity-100 w-full max-w-[300px]">
 							<option value="">None</option>
 							<option value="Male">Male</option>
 							<option value="Female">Female</option>
 				</select>
 				<h3 className="py-2 text-lg">Age</h3>
 				{/*<Dropdown queryType="age" callback={filterItems} cats={cats} isInsidePanel={true}/>*/}
-				<select id="sort" value={ageType} onChange={(e) => setAgeType(e.target.value)} className=" drop-shadow-md p-2 text-xl rounded-xl bg-[#e5e5ff] bg-opacity-100">
+				<select id="sort" value={ageType} onChange={(e) => setAgeType(e.target.value)} className=" drop-shadow-md p-2 text-xl rounded-xl bg-[#e5e5ff] bg-opacity-100 w-full max-w-[300px]">
 					<option value="">None</option>
 					{ageGroups.map((age) => (
 						<option value={age.age}>{age.age}</option>
@@ -221,7 +272,7 @@ export default function Page() {
 				</select>
 				<h3 className="py-2 text-lg">Color</h3>
 				{/*<Dropdown queryType="color" callback={filterItems} cats={cats} isInsidePanel={true}/>*/}
-				<select id="sort" value={colorType} onChange={(e) => setColorType(e.target.value)} className=" drop-shadow-md p-2 text-xl rounded-xl bg-[#e5e5ff] bg-opacity-100">
+				<select id="sort" value={colorType} onChange={(e) => setColorType(e.target.value)} className=" drop-shadow-md p-2 text-xl rounded-xl bg-[#e5e5ff] bg-opacity-100 w-full max-w-[300px]">
 					<option value="">None</option>
 					{colors.map((color) => (
 						<option value={color}>{color}</option>
@@ -242,34 +293,47 @@ export default function Page() {
 			<div className="flex w-full">
 
 				<div className="w-full h-full">
-					{/* Search Field */}
+					{/* Search Bar -- F */}
 					<div className="align-middle justify-center w-full flex">
-						<input type="text"
-							name="catlist-search"
-							placeholder="Search"
-							value={fieldInput}
-							className=" bg-[#e5e5ff] bg-opacity-50 dark:bg-gray-300 dark:bg-opacity-100 placeholder-text-header-0 shadow drop-shadow-lg rounded-3xl text-xl pl-4 w-full h-16"
-							onChange = { (Event) => searchItems(Event.target.value, "") }>
-						</input>
+						<div className="w-full relative">
+							<input type="text"
+								name="catlist-search"
+								id="search-bar"
+								placeholder="Search"
+								value={fieldInput}
+								className=" bg-[#e5e5ff] bg-opacity-50 dark:bg-gray-300 dark:bg-opacity-100 placeholder-text-header-0 shadow drop-shadow-lg rounded-3xl text-xl pl-4 w-full h-16"
+								onChange = { (Event) => searchItems(Event.target.value, "") }>
+							</input>
+
+							{ filteredResults && filteredResults.length > 0 && fieldInput.length > 0 && activeAutocomplete ? 
+							<div ref={searchbarRef} className={`absolute w-full z-50 bg-gray-100 bg-opacity-100 border-2 placeholder-text-header-0 shadow rounded-3xl text-xl flex-col overflow-hidden`}>
+								{
+									filteredResults.map((cat) => (
+										<button className="w-full text-left h-10 hover:bg-white pl-4" onClick={() => completeAutocomplete(cat.name)}>{cat.name}</button>
+									))
+								}
+							</div> : <div />
+							}
+						</div>
 						
 						<Image className="relative -translate-x-12" alt="Search..." src="/img/search-icon.svg" width={30} height={30} />
 					</div>
-					{ filteredResults && filteredResults.length > 0 && fieldInput.length > 0 && activeAutocomplete ? 
-						<div className="absolute z-50 bg-gray-100 bg-opacity-100 border-2 placeholder-text-header-0 shadow rounded-3xl text-xl w-4/5 justify-center flex-col m-auto left-[10%] translate-x-2 translate-y-1 overflow-hidden">
-							{
-								filteredResults.map((cat) => (
-									<button className="w-full text-left h-10 hover:bg-white pl-4" onClick={() => completeAutocomplete(cat.name)}>{cat.name}</button>
-								))
-							}
-						</div> : <div />
-					}
 				</div>
 
-				<div className=" w-full max-w-[200px] mr-full ml-auto justify-end flex-col bg-white dark:bg-gray-500 rounded-xl p-4 drop-shadow-lg">
+				<div className=" w-full max-w-[200px] mr-full ml-auto justify-end flex-col bg-white dark:bg-gray-500 rounded-xl p-4 drop-shadow-lg z-50">
 					<h2 className="flex justify-start font-bold text-xl text-text-header-0 drop-shadow-md">Sort by:</h2>
-					<div className=" pt-4">
+					
+					{/*<div className=" pt-4">
 						<Dropdown queryType="sort" callback={sortItems} isInsidePanel={true}/>
-					</div>
+					</div>*/}
+
+					{/* Sorting Dropdown Filter -- F */}
+					<select id="sort" value={sortingMethod} onChange={(e) => setSortingMethod(e.target.value)} className=" drop-shadow-md p-2 mt-4 text-xl rounded-xl bg-[#e5e5ff] bg-opacity-100">
+						<option value="" >None</option>
+						{ sortbyGroups.map((filterType) => (
+							<option value={filterType.group}>{filterType.group}</option>
+						))}
+					</select>
 				</div>
 				{dbUser && dbUser.role === "breeder" &&
 				<div className="absolute size-fit right-0 top-[40px]">

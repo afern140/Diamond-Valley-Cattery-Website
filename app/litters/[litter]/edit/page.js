c17a8e1afb7528585be553bdb2758f58d7c9722b
tansimafiana <tansima.fiana@edu.sait.ca>
2024-04-10 09:24:54 -0600

    addChanges

--- app/litters/[litter]/edit/page.js
+++ app/litters/[litter]/edit/page.js
@@ -279,7 +279,7 @@ export default function Page({ params }) {
 					<div className="">
 						<LitterCarouselController onImageUpload={handleImageUpload} litter={litter} />
 					</div>
-					<button className="flex m-auto px-6 py-4 bg-white drop-shadow-lg rounded-xl mt-16 text-2xl" onClick={handleSubmit}>Submit</button>
+					<button className="flex m-auto px-6 py-4 bg-navbar-body-0 text-white drop-shadow-lg rounded-xl mt-16 text-2xl" onClick={handleSubmit}>Submit</button>
 				</div>
 			) : (
 				<div className="h-screen">

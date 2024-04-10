7dba092e5e077d24b00b2f70e1e0bbd6ff4022d0
tansimafiana <tansima.fiana@edu.sait.ca>
2024-04-10 09:29:45 -0600

    addChanges

--- app/litters/[litter]/edit/page.js
+++ app/litters/[litter]/edit/page.js
@@ -281,16 +281,8 @@ export default function Page({ params }) {
 							</div>
 						</div>
 					)}
-
-					{
-					<div className="">
-						<LitterCarouselController onImageUpload={handleImageUpload} litter={litter} />
-					</div>
-					}
-
-					{
 					<button className="flex m-auto px-6 py-4 bg-navbar-body-0 text-white drop-shadow-lg rounded-xl mt-16 text-2xl" onClick={handleSubmit}>Submit</button>
-					}
+					<LitterCarouselController onImageUpload={handleImageUpload} litter={litter} />
 				</div>
 			) : (
 				<div className="h-screen">

f2bb58fc2a0de1b7f09a5c8ee2c5c33b9256fe2b
tansimafiana <tansima.fiana@edu.sait.ca>
2024-04-10 09:18:12 -0600

    addMinorChange

--- app/litters/[litter]/edit/page.js
+++ app/litters/[litter]/edit/page.js
@@ -277,6 +277,7 @@ export default function Page({ params }) {
 							<CatSelection cats={cats} showCatSelection={showChildSelection} setShowCatSelection={setShowChildSelection} handleSelectCat={handleSelectChild}/>
 						</div>
 					)}
+					</div>
 					<button className="flex m-auto px-6 py-4 bg-white drop-shadow-lg rounded-xl mt-16 text-2xl" onClick={handleSubmit}>Submit</button>
 					<LitterCarouselController onImageUpload={handleImageUpload} litter={litter} />
 				</div>

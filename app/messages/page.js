0f3ff6f1a328231ce597c2f5b3da6e31a8d7ae55
tansimafiana <tansima.fiana@edu.sait.ca>
2024-04-10 19:42:54 -0600

    addChanges

--- app/messages/page.js
+++ app/messages/page.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import { useUserAuth } from "@/app/_utils/auth-context";
 import { useChat } from "@/app/_utils/chat-context";
 import Link from "next/link";
+import BackgroundUnderlay from "@/app/components/background-underlay";
 
 //Messagelist
 export default function page() {
@@ -29,21 +30,31 @@ export default function page() {
     return date.toLocaleString();
   };
   return (
-    <div>
-      <h2>Messages</h2>
-      {allMessages.map(({ chatId, messages }) => (
-        <div key={chatId}>
-          {messages.map((message) => (
-            <Link key={message.id} href={`/messages/${chatId}`}>
-              <div className="block p-2 hover:bg-gray-200 cursor-pointer">
-                {message.displayName}: {message.text}
-                <br />
-                <small>{formatTimestamp(message.timestamp)}</small>
-              </div>
-            </Link>
-          ))}
+    <div className="relative text-header-text-0 min-h-[66vh] h-fit">
+      <BackgroundUnderlay />
+
+      {/* Header */}
+      <div className="pt-20 flex pb-10">
+        <div className="w-4/5 m-auto justify-center flex-col text-center mx-auto inline-block font-bold bg-[#092C48] dark:bg-dark-header-text-0 text-transparent bg-clip-text pb-2">
+          <span className="text-6xl pb-10 font-extrabold">Messages</span> <br />
         </div>
-      ))}
+      </div>
+      <div className="w-4/5 bg-white mx-auto p-10 dark:bg-gray-500 relative rounded-xl drop-shadow-lg">
+        {allMessages.map(({ chatId, messages }) => (
+          <div key={chatId} className="relative z-10 bg-navbar-body-1 p-4 rounded-xl drop-shadow-lg">
+            {messages.map((message) => (
+              <Link key={message.id} href={`/messages/${chatId}`}>
+                <div className="block p-2 hover:bg-gray-200 cursor-pointer">
+                  {message.displayName}: {message.text}
+                  <br />
+                  <small>{formatTimestamp(message.timestamp)}</small>
+                </div>
+              </Link>
+              
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
